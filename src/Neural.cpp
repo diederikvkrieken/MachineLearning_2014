06514f4313d9199525863d8e67733a21a3d64640
Diederik <diederikvkrieken@gmail.com>
2014-12-30 13:56:01 +0100

    Added node value, to save values of nodes.

--- src/Neural.cpp
+++ src/Neural.cpp
@@ -12,14 +12,20 @@ void NN::initializeNN()
 {
   for(int i=0; i < n_hidden; i++)
   {
-    // Initialise hidden nodes
-    for(int j=0; j < n_input; j++)
-    {
-      wHL[i][j] = randInt(0, 100) / 100.0;
-    }
+    // Initialise hidden nodes values
+    nHL[i] = randInt(0, 100) / 100.0;
 
     // Initialise hidden nodes bias
     bHL[i] = randInt(0, 100) / 100.0;
+
+    // Initialise hidden nodes weights
+    for(int j=0; j < n_input; j++)
+    {
+      wHL[i][j] = randInt(0, 100) / 100.0;
+      // Initialise input nodes values
+      nIL[j] = randInt(0, 100) / 100.0;
+    }
+
 
     // Initialise output nodes
     for(int j=0; j < n_output; j++)
@@ -27,6 +33,8 @@ void NN::initializeNN()
       wOL[j][i] = randInt(0, 100) / 100.0;
       // Initialise output nodes bias
       bOL[j] = randInt(0, 100) / 100.0;
+      // Initialise output nodes value
+      nOL[j] = randInt(0, 100) / 100.0;
     }
   }
 }
